ee1db52b9332f3fdd4d7be79eb9d6c341331f236
Silvio Daniel Martinez <silviodaniel3@gmail.com>
2017-06-28 19:56:41 -0400

    Updated code, clean up later
    This works, but I realize it can be improved once I learn the way to create an empty data frame and add to it in pandas!

--- Survey Sleep Data.py
+++ Survey Sleep Data.py
@@ -7,18 +7,24 @@ Created on Wed Jun 28 09:55:34 2017
 
 #Next task: trying to extract survey data on sleep and store into data frame for all days
 import pandas as pd
-columns=['question id','question type','question text','question answer options','answer']
+import glob
+columns=['question id','question type','question text','question answer options','Sleep Times']
 survey=pd.read_csv('C:/Users/Silvio/Documents/Python/v3dw1iq/survey_answers/Morning/2017-06-21 15_43_46.csv',header=0,sep=',',names=columns)
 survey.drop(survey.columns[[0,1,2,3]],axis=1,inplace=True)
-#ans=sleep['answer'].values
-#answers=[ans[1], ans[2]]#will create a list, but how do we want to store them?
-type(survey)
-survey.drop(survey.index[[0,3,4,5,6,7,8,9,10,11]])
-sleep=survey
+#**wanted to define this data frame before the loop in order to append to this data frame,
+#could also learn to append to an empty data frame!
+sleep=survey.drop(survey.index[[0,3,4,5,6,7,8,9,10,11]])
 sleep
-ans
-
-columns=['Questions','Answers']
-df_ = pd.DataFrame(columns=columns)
-df=pd.DataFrame(sleep,columns=columns)
-pd.DataFrame.add()
+list_of_files=glob.glob('C:/Users/Silvio/Documents/Python/v3dw1iq/survey_answers/Morning/*.csv')
+c=0
+for file in list_of_files:#loop through to add sleep times of each day to Sleep Times data frame
+    columns=['question id','question type','question text','question answer options','Sleep Times']#make sure these column names match the previously defined names!
+    survey=pd.read_csv(file,header=0,sep=',',names=columns)
+    survey.drop(survey.columns[[0,1,2,3]],axis=1,inplace=True)
+    store=survey.drop(survey.index[[0,3,4,5,6,7,8,9,10,11]])
+    sleep = sleep.append(store, ignore_index=True)
+    c+=1#count number of files
+sleep=sleep.drop(sleep.index[[0,1]])#removing the repeated file that was read in the loop
+c#number of files
+sleep.index=sleep.index-1
+print(sleep)
